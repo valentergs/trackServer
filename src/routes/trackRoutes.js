7ee566ee63ee5f575f55947f85df80943c5b8092
Rodrigo Valente <rodrigovalente@hotmail.com>
2019-08-21 14:25:31 -0300

    add trackRoutes post route

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -13,4 +13,22 @@ router.get("/tracks", async (req, res) => {
   const tracks = await Track.find({ userId: req.user._id });
   res.send(tracks);
 });
+
+router.post("/tracks", async (req, res) => {
+  const { name, locations } = req.body;
+  if (!name || !locations) {
+    return res
+      .status(422)
+      .send({ error: "Você deve fornecer um nome e uma localização" });
+  }
+
+  try {
+    const track = new Track({ name, locations, userId: req.user._id });
+    await track.save();
+    res.send(track);
+  } catch (error) {
+    res.status(422).send({ error: error.message });
+  }
+});
+
 module.exports = router;
